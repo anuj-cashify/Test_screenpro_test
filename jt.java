d4109c445f623548b0a4267dc202a73ac284a3a9
Anuj Gupta <anuj.g@cashify.in>
2021-05-03 16:31:25 +0530

    inheritance

--- jt.java
+++
@@ -1,7 +0,0 @@
-package TestPackage;
-
-public class jt {
-public static void main(String[] args) {
-	System.out.println("Hello Java");
-}
-}
---
+++ testinheritance.java
@@ -0,0 +1,12 @@
+package TestPackage;
+
+public class testinheritance {
+	 
+		public static void main(String args[]){  
+		Man d=new Man();  
+		d.bark();  
+		d.eat();  
+		}
+
+	}
+
