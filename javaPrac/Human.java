3f5b88a93209540b37afe242f7af093d9502d0d2
Anuj Gupta <anuj.g@cashify.in>
2021-05-05 19:28:57 +0530

    my java

---
+++ javaPrac/Human.java
@@ -0,0 +1,6 @@
+package TestPackage;
+
+public class Human {
+	void eat(){System.out.println("Male...");}  
+
+}
---
+++ javaPrac/Man.java
@@ -0,0 +1,5 @@
+package TestPackage;
+
+public class Man extends Human{  
+void bark(){System.out.println("Anuj");}  
+}  
